1d1f8fadda4e832a1131223237b2a1c86544e7d9
nrht <i.norihi.472@gmail.com>
2021-07-11 21:09:47 +0900

    make it possible to use dark/light theme

--- stream-me/pages/_app.tsx
+++ stream-me/pages/_app.tsx
@@ -1,4 +1,15 @@
 import App from 'next/app';
 import React from 'react';
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
 
-export default ({ Component, pageProps }) => <Component {...pageProps} />;
+import { themeDark, themeLight } from 'lib/theme';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={false ? themeDark : themeLight}>
+      <CssBaseline />
+      <Component {...pageProps} />)
+    </ThemeProvider>
+  );
+}
